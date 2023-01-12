9e577fab2a5ccba62a1ceb860b48c3159e247139
rhoadeschris <54925442+rhoadeschris@users.noreply.github.com>
2023-01-12 16:24:02 -0600

    files

---
+++ data/slide11.js
@@ -0,0 +1,6 @@
+(function(){var loadHandler=window['sl_{B55B768E-ADB2-4729-8182-0C28730AFECD}'];loadHandler&&loadHandler(10, '<div id="spr0_7ac2c2f"><div id="spr1_7ac2c2f" class="kern slide"><img id="img1_7ac2c2f" src="data/img0.png" width="1280px" height="720px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_7ac2c2f" class="kern slide"><div id="spr3_7ac2c2f" style="left:88px;top:38.333px;"><div style="width:0px;"><span id="txt0_7ac2c2f" data-width="724.218750" style="left:9.6px;top:37.34px;">Find the 90% confidence interval.</span></div></div><div id="spr4_7ac2c2f" style="left:100.5px;top:205.5px;"><img id="img0_7ac2c2f" src="data/img5.png" width="1150.5" height="369" alt=" Calculations: c=.90, S.D. = .16, Mean = 12.1, n = 16\
+\
+ Test Statistic (Z* or ZC ) = invnorm(.05, 0, 1) = 1.645\
+\
+𝐂𝐈=𝟏𝟐.𝟏 ±𝟏.𝟔𝟒𝟓  .𝟏𝟔   𝟏𝟔    = (12.034 , 12.166)\
+"/></div></div></div>', '{"s":[]}');})();
